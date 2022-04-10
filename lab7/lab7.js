6a11ace808ca018ee3a9e9a6aab2ce65807780c0
Никита Гордеев <nagordeev0106@yandex.ru>
2022-04-10 19:49:35 +0300

    lab7Update4

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -1,19 +1,19 @@
-const change_background = (code) => {
+const changeBackground = (code) => {
     switch (code.slice(0, 2)) {
         case '01':
-            document.body.style.background = "url(./img/clear_sky.jpg) no-repeat top center";
+            document.body.style.background = "url(./img/clearSky.jpg) no-repeat top center";
             return;
         case '02':
-            document.body.style.background = "url(./img/few_clouds.jpg) no-repeat top center";
+            document.body.style.background = "url(./img/fewClouds.jpg) no-repeat top center";
             return;
         case '03':
-            document.body.style.background = "url(./img/scattered_clouds.jpg) no-repeat top center"
+            document.body.style.background = "url(./img/scatteredClouds.jpg) no-repeat top center"
             return;
         case '04':
-            document.body.style.background = "url(./img/broken_clouds.jpg) no-repeat top center"
+            document.body.style.background = "url(./img/brokenClouds.jpg) no-repeat top center"
             return;
         case '09':
-            document.body.style.background = "url(./img/shower_rain.jpg) no-repeat top center";
+            document.body.style.background = "url(./img/showerRain.jpg) no-repeat top center";
             return;
         case '10':
             document.body.style.background = "url(./img/rain.jpg) no-repeat top center";
@@ -31,88 +31,85 @@ const change_background = (code) => {
 }
 
 
-const weather_now_func = (info) => {
-    change_background(info.weather[0].icon)
+const weatherNowFunc = (info) => {
+    changeBackground(info.weather[0].icon)
     const temp = Math.round(info.main.temp);
-    document.getElementById("main_temp").innerHTML = (temp > 0 ? '+' : temp < 0 ? '-' : '') + temp + "°"
-    const feels_like = Math.round(info.main.feels_like)
-    document.getElementById("main_feels_like").innerHTML = "Ощущается как: " + (feels_like > 0 ? '+' : feels_like < 0 ? '-' : '') + feels_like + "°"
-    document.getElementById("main_description").innerHTML = info.weather[0].description[0].toUpperCase() + info.weather[0].description.slice(1)
-    document.getElementById("main_picture").src = `https://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`
-    document.getElementById("main_wind").innerHTML = `Скорость ветра: ${Math.round(info.wind.speed)}м/с`
-    document.getElementById("main_wet").innerHTML = `Относительная влажность: ${info.main.humidity}%`
-    document.getElementById("main_pressure").innerHTML = `Атмосферное давлние: ${Math.round(info.main.pressure * 0.75)}мм рт. ст.`
+    document.getElementById("mainTemp").innerHTML = (temp > 0 ? '+' : temp < 0 ? '-' : '') + temp + "°"
+    const feelsLike = Math.round(info.main.feels_like)
+    document.getElementById("mainFeelsLike").innerHTML = "Ощущается как: " + (feelsLike > 0 ? '+' : feelsLike < 0 ? '-' : '') + feelsLike + "°"
+    document.getElementById("mainDescription").innerHTML = info.weather[0].description[0].toUpperCase() + info.weather[0].description.slice(1)
+    document.getElementById("mainPicture").src = `https://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`
+    document.getElementById("mainWind").innerHTML = `Скорость ветра: ${Math.round(info.wind.speed)}м/с`
+    document.getElementById("mainWet").innerHTML = `Относительная влажность: ${info.main.humidity}%`
+    document.getElementById("mainPressure").innerHTML = `Атмосферное давлние: ${Math.round(info.main.pressure * 0.75)}мм рт. ст.`
 }
 
-const weather_next_func = (info) => {
-    let bottom = document.getElementById("bottom_box")
+const weatherNextFunc = (info) => {
+    let bottom = document.getElementById("bottomBox")
     if (bottom) bottom.remove()
     bottom = document.createElement("div")
-    bottom.setAttribute('id', 'bottom_box')
+    bottom.setAttribute('id', 'bottomBox')
     const date = (new Date).getHours()
     for (hour = 1; hour < 24; hour++) {
-        const next_temp = document.createElement('div')
-        next_temp.setAttribute('id', 'next_temp')
-        next_temp.innerHTML = (Math.round(info[hour].temp) > 0 ? '+' : Math.round(info[hour].temp) < 0 ? '-' : '') + Math.round(info[hour].temp) + "°"
+        const nextTemp = document.createElement('div')
+        nextTemp.setAttribute('id', 'nextTemp')
+        nextTemp.innerHTML = (Math.round(info[hour].temp) > 0 ? '+' : Math.round(info[hour].temp) < 0 ? '-' : '') + Math.round(info[hour].temp) + "°"
 
-        const next_picture = document.createElement('img')
-        next_picture.setAttribute('id', "next_picture")
-        next_picture.setAttribute('src', `https://openweathermap.org/img/wn/${info[hour].weather[0].icon}@2x.png`)
+        const nextPicture = document.createElement('img')
+        nextPicture.setAttribute('id', "nextPicture")
+        nextPicture.setAttribute('src', `https://openweathermap.org/img/wn/${info[hour].weather[0].icon}@2x.png`)
 
         const time = document.createElement('div')
         time.setAttribute('id', "time")
         time.innerHTML = (date + hour) % 24 + ':00'
 
-        const next_description = document.createElement('div')
-        next_description.setAttribute('id', "next_description")
-        next_description.innerHTML = info[hour].weather[0].description[0].toUpperCase() + info[hour].weather[0].description.slice(1)
+        const nextDescription = document.createElement('div')
+        nextDescription.setAttribute('id', "nextDescription")
+        nextDescription.innerHTML = info[hour].weather[0].description[0].toUpperCase() + info[hour].weather[0].description.slice(1)
 
-        const next_wind = document.createElement('div')
-        next_wind.setAttribute('class', "item")
-        next_wind.setAttribute('id', "next_wind")
-        next_wind.innerHTML = `<i class='fa-solid fa-wind'></i><div>${Math.round(info[hour].wind_speed)}м/с</div>`
+        const nextWind = document.createElement('div')
+        nextWind.setAttribute('class', "item")
+        nextWind.setAttribute('id', "nextWind")
+        nextWind.innerHTML = `<i class='fa-solid fa-wind'></i><div>${Math.round(info[hour].wind_speed)}м/с</div>`
 
-        const next_wet = document.createElement('i')
-        next_wet.setAttribute('class', "item")
-        next_wet.setAttribute('id', "next_wet")
-        next_wet.innerHTML = `<i class='fa-solid fa-droplet'></i><div>${info[hour].humidity}%</div>`
+        const nextWet = document.createElement('i')
+        nextWet.setAttribute('class', "item")
+        nextWet.setAttribute('id', "nextWet")
+        nextWet.innerHTML = `<i class='fa-solid fa-droplet'></i><div>${info[hour].humidity}%</div>`
 
-        const next_pressure = document.createElement('div')
-        next_pressure.setAttribute('class', "item")
-        next_pressure.setAttribute('id', "next_pressure")
-        next_pressure.innerHTML = `<i class='fa-regular fa-compass'></i><div>${Math.round(info[hour].pressure * 0.75)}мм рт. ст.</div>`
+        const nextPressure = document.createElement('div')
+        nextPressure.setAttribute('class', "item")
+        nextPressure.setAttribute('id', "nextPressure")
+        nextPressure.innerHTML = `<i class='fa-regular fa-compass'></i><div>${Math.round(info[hour].pressure * 0.75)}мм рт. ст.</div>`
 
         const left = document.createElement('div')
-        left.setAttribute('id', 'next_left')
-        left.append(...[next_temp, next_picture])
+        left.setAttribute('id', 'nextLeft')
+        left.append(nextTemp, nextPicture)
         const right = document.createElement('div')
-        right.setAttribute('id', 'next_right')
-        right.append(...[time, next_description, next_wind, next_wet, next_pressure])
+        right.setAttribute('id', 'nextRight')
+        right.append(time, nextDescription, nextWind, nextWet, nextPressure)
 
         const box = document.createElement('div')
-        box.setAttribute('class', 'box_next_weather')
-        box.append(...[left, right])
+        box.setAttribute('class', 'boxNextWeather')
+        box.append(left, right)
         bottom.append(box)
     }
     document.body.insertBefore(bottom, document.getElementById("script"))
 }
 
 main = () => {
-    /*console.log("----------------------------------------")*/
-    const city = input_city.value === "" ? "Москва" : encodeURIComponent(input_city.value)
-    const api_key = "ca145850eea033fbad59a1c102308f58"
+    const city = inputCity.value === "" ? "Москва" : encodeURIComponent(inputCity.value)
+    const apiKey = "ca145850eea033fbad59a1c102308f58"
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${api_key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`)
         .then(response => response.json())
         .then(result => {
-            /*console.log(result)*/
             if (result.cod != 404) {
-                weather_now_func(result)
-                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${result.coord.lat}&lon=${result.coord.lon}&lang=ru&cnt=5&units=metric&appid=${api_key}`)
-                    .then(response_in => response_in.json())
-                    .then(result_in => {
-                        /*console.log(result_in);*/
-                        (result_in.cod != 404) && weather_next_func(result_in.hourly)
+                weatherNowFunc(result)
+                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${result.coord.lat}&lon=${result.coord.lon}&lang=ru&cnt=5&units=metric&appid=${apiKey}`)
+                    .then(responseIn => responseIn.json())
+                    .then(resultIn => {
+                        (resultIn.cod != 404) && weatherNextFunc(resultIn.hourly)
                     }, error => alert(error))
             } else alert("Город не найден")
         }, error => alert(error))
