38bcbf71cfe0d836132044f49478c3a1aba0379f
Никита Гордеев <nagordeev0106@yandex.ru>
2022-04-17 22:52:27 +0300

    lab8Done

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Task from './task'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [valueToDo, setValue] = useState('')
+    const [toDos, setToDo] = useState([])
+    const [count, oneMore] = useState(1)
+
+    const makeToDo = () => {
+        if (valueToDo.trim()) {
+            setToDo([...toDos, {
+                id: count,
+                title: valueToDo,
+                done: false
+            }])
+            oneMore((prev) => prev = prev + 1)
+        } else alert('Нет задачи')
+        setValue('')
+    }
+
+    const changeToDo = (idToDo, chngValue) => {
+        setToDo(
+            toDos.map((toDo) => (
+                toDo.id === idToDo
+                    ? { ...toDo, title: chngValue }
+                    : { ...toDo }
+            ))
+        )
+    }
+
+    const changeStatus = (idToDo) => {
+        setToDo(
+            toDos.map((toDo) => (
+                toDo.id === idToDo
+                    ? { ...toDo, done: !toDo.done }
+                    : { ...toDo }
+            ))
+        )
+    }
+    //console.log(toDos)
+    return (
+        <div className="App">
+            <span className="Name">To Do</span>
+            <form className="input">
+                <input
+                    type="text"
+                    className="newToDo"
+                    placeholder="Введите новую задачу..."
+                    value={valueToDo}
+                    onChange={(event) => setValue(event.target.value)}
+                />
+                <input
+                    type="button"
+                    className="makeToDo"
+                    value="Добавить"
+                    onClick={() => makeToDo()}
+                />
+            </form>
+            <div className="toDoList">
+                {toDos.map(toDo => (
+                    <Task
+                        key={toDo.id}
+                        toDo={toDo}
+                        changeStatus={changeStatus}
+                        changeToDo={changeToDo}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
